3d9021dcd31394d5127ad7cfc054d7baacc13cb3
AllanDeyvison <allandeyvison007dias@hotmail.com>
2021-06-08 20:02:06 -0300

    Implementação do post e get de postagens

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -21,6 +21,10 @@ export class AuthService {
     return this.http.post<User>('https://blogpessoalgenerationheroku.herokuapp.com/usuarios/cadastrar',user)
   }
 
+  getByIdUser(id: number): Observable<User>{
+    return this.http.get<User>(`https://blogpessoalgenerationheroku.herokuapp.com/usuarios/id/${id}`)
+  }
+
 logado(){
   let ok: boolean = false
 
